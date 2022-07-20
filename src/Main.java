25f32da0100fe7a48a843d1e0696b82dda7402dc
origin <git@github.com:Julianaportehla/desafio_poo_dio.git>
2022-07-19 22:06:12 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import dominio.Curso;
+import dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso javaScript");
+        curso2.setDescricao("Descrição curso javaScript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
